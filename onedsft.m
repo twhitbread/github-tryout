08060b2f6e4063f04f3a9131113840404a6f73fb
Tim Whitbread <tim.j.whitbread>
2015-12-02 15:08:12 +0000

    Much quicker version - data stored in files

--- onedsft.m
+++ onedsft.m
@@ -1,29 +1,28 @@
 clear all
-%clearvars -except slnew bip_day
 Rsun = 696000;
-tau = 10*24*365;
-eta = 1800000;
+tau = 10*2*365.25;
+eta = 21600000;
 maxl = 128;
 theta = 0:pi/180:pi;
 B0 = 10;
-v0 = 57.6;
+v0 = 691.2;
 v = -sin(2*theta).*exp(pi-2.*abs(pi/2 - theta));
 v0 = v0/max(abs(v));
 v = v*v0;
 Binit = B0*sign(pi/2-theta).*exp(-exp(pi-2.*abs(pi/2-theta)).*v0/(4*eta).*(sin(2.*abs(pi/2-theta))+cos(2.*abs(pi/2-theta))));
 
-[bip_day slnew] = bmr;
+%[bip_day slnew] = bmr;
+
+load('slnew.mat')
+load('bip_day.mat')
+load('leg.mat')
+load('Yl.mat')
 
 % Store Legendre polynomials and calculate al(0)
 
-leg = zeros(maxl+1,length(theta));
-Yl = zeros(maxl+1,length(theta));
 alth = zeros(maxl+1,length(theta));
 
 for l=0:maxl
-     m = legendre(l,cos(theta));
-     leg(l+1,:) = m(1,:);
-     Yl(l+1,:) = sqrt((2*l+1)/(4*pi))*leg(l+1,:);
      alth(l+1,:) = Yl(l+1,:).*Binit.*sin(theta);
 end
 
@@ -55,7 +54,6 @@ for l=0:maxl
       Cll(l+1,lp+1) = trapz(theta,q');
       Cll(l+1,lp+1) = Cll(l+1,lp+1)*(2*pi/Rsun)*sqrt((2*l+1)/(4*pi))*sqrt((2*lp+1)/(4*pi));
    end
-   %Mll = diag(-eta*l.*(l+1)/(Rsun^2) - invtau);% - Cll;
 end
 
 l=0:maxl;
@@ -73,16 +71,16 @@ Mll = Mll - Cll;
 A = [];
 T = [];
 
-[T1,A1] = ode45(@odematrix,0:5232,ainit,[],Mll);
+[T1,A1] = ode45(@odematrix,0:436,ainit,[],Mll);
 A = [A;A1];
 T = [T,T1];
 
 sz1 = size(A1);
 abmr = A1(sz1(1),:)+slnew(1,:);
-[T1,A1] = ode45(@odematrix,5232:5544,abmr,[],Mll);
+[T1,A1] = ode45(@odematrix,436:462,abmr,[],Mll);
 A = [A;A1];
 T = [T;T1];
-iprev = 231*24;
+iprev = 231*2;
 sza = size(A);
 
 for i = 231:12564
@@ -91,19 +89,12 @@ for i = 231:12564
             if bip_day(j) == bip_day(j+1)
                 slnew(j+1,:) = slnew(j+1,:) + slnew(j,:);
                 bip_day(j) = bip_day(j-1);
-                continue
-            elseif bip_day(j)-bip_day(j-1) ~= 1
-                [T1,A1] = ode45(@odematrix,iprev:bip_day(j+1)*24,A(sza(1),:)+slnew(j,:),[],Mll); 
-                A = [A;A1];
-                T = [T;T1];
-                sza = size(A);
-                iprev = bip_day(j+1)*24; 
             else
-                [T1,A1] = ode45(@odematrix,bip_day(j)*24:bip_day(j+1)*24,A(sza(1),:)+slnew(j,:),[],Mll);
+                [T1,A1] = ode45(@odematrix,iprev:bip_day(j+1)*2,A(sza(1),:)+slnew(j,:),[],Mll); 
                 A = [A;A1];
                 T = [T;T1];
                 sza = size(A);
-                iprev = bip_day(j+1)*24;
+                iprev = bip_day(j+1)*2; 
             end
         end
     end
@@ -111,8 +102,8 @@ end
 
 % Final BMR
 
-if iprev == 12730*24;
-[T1,A1] = ode45(@odematrix,iprev:13000*24,A(sza(1),:)+slnew(5474,:),[],Mll); 
+if iprev == 12730*2;
+[T1,A1] = ode45(@odematrix,iprev:13000*2,A(sza(1),:)+slnew(5474,:),[],Mll); 
 A = [A;A1];
 T = [T;T1];
 end
@@ -125,11 +116,10 @@ pcolor(B')
 title('Longitude-Averaged B_r','FontSize',14)
 shading interp
 colorbar
-caxis([-10,10])
+caxis([-6,6])
 xlabel('Time (years)','FontSize',14)
-set(gca,'XTick',1:48833:292999)
+set(gca,'XTick',1:4267:25603)
 set(gca, 'XTickLabel',0:5:30)
 ylabel('Latitude (degrees)','FontSize',14)
 set(gca, 'YTick',[1;91;181])
 set(gca, 'YTickLabel', [-90;0;90])
-
